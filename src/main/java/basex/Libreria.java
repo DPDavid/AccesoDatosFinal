0fc8fa8a389b01fcaed35edb7dc7e25dc5ed02d0
David Diaz <diazperezdavid755@gmail.com>
2025-03-05 13:49:03 +0100

    Creacion de colecciones y correccion del CRUD

--- src/main/java/basex/Libreria.java
+++ src/main/java/basex/Libreria.java
@@ -4,6 +4,11 @@ import javafx.scene.control.Alert;
 import org.basex.api.client.ClientQuery;
 import org.basex.api.client.ClientSession;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Libreria {
     private ClientSession session;
 
@@ -29,9 +34,11 @@ public class Libreria {
         }
     }
 
-    public void insertarLibro(String libroXML) {
+    public void insertarLibro(String nombreColeccion, String libroXML) {
         try {
-            String xquery = "insert node " + libroXML + " into /Libreria/Libros";
+            String xquery = "let $doc := doc('libreria/" + nombreColeccion.trim() + "') " +
+                    "return insert node " + libroXML + " into $doc/Libreria/Libros";
+
             ejecutarConsulta(xquery);
             mostrarAlerta("Éxito", "Libro agregado correctamente.", Alert.AlertType.INFORMATION);
         } catch (Exception e) {
@@ -40,14 +47,9 @@ public class Libreria {
         }
     }
 
-    public String obtenerLibroPorTitulo(String titulo) {
-        String xquery = "for $libro in /Libreria/Libros/Libro[Titulo='" + titulo + "'] return $libro";
-        return ejecutarConsulta(xquery);
-    }
-
-    public void actualizarPrecio(String titulo, double nuevoPrecio) {
+    public void actualizarPrecio(String nombreColeccion, String titulo, double nuevoPrecio) {
         try {
-            String xquery = "replace value of node /Libreria/Libros/Libro[Titulo='" + titulo + "']/Precio with '" + nuevoPrecio + "'";
+            String xquery = "let $doc := doc('libreria/"+nombreColeccion.trim()+"') return replace value of node /Libreria/Libros/Libro[Titulo='" + titulo + "']/Precio with '" + nuevoPrecio + "'";
             ejecutarConsulta(xquery);
             mostrarAlerta("Éxito", "Libro actualizado correctamente.", Alert.AlertType.INFORMATION);
         } catch (Exception e) {
@@ -56,9 +58,10 @@ public class Libreria {
         }
     }
 
-    public void eliminarLibro(String titulo) {
+    public void eliminarLibro(String nombreColeccion, String titulo) {
         try {
-            String xquery = "delete node /Libreria/Libros/Libro[Titulo='" + titulo + "']";
+            String xquery = "let $doc := doc('libreria/" + nombreColeccion.trim() + "') " +
+                    "return delete node $doc/Libreria/Libros/Libro[Titulo='" + titulo + "']";
             ejecutarConsulta(xquery);
             mostrarAlerta("Éxito", "Libro eliminado correctamente.", Alert.AlertType.INFORMATION);
         } catch (Exception e) {
@@ -67,11 +70,47 @@ public class Libreria {
         }
     }
 
-    public String listarLibros() {
-        String xquery = "for $libro in /Libreria/Libros/Libro " +
+    public String listarLibros(String nombreColeccion) {
+        String xquery = "for $libro in doc('libreria/" + nombreColeccion.trim() + "')/Libreria/Libros/Libro " +
                 "return concat('Título: ', $libro/Titulo, ', Autor: ', $libro/Autor, ', Género: ', $libro/Genero, ', Precio: ', $libro/Precio)";
         return ejecutarConsulta(xquery);
     }
+
+    public void agregarDocumentoAColeccion(String nombreColeccion, String archivoXML) {
+        try {
+            String xquery = "ADD TO " + nombreColeccion+ " "+ archivoXML;
+            session.execute(xquery);
+            mostrarAlerta("Éxito", "Documento añadido correctamente a la colección.", Alert.AlertType.INFORMATION);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void eliminarColeccion(String nombreDocumento) {
+        try {
+            String comando = "DELETE " + nombreDocumento;
+            session.execute(comando);
+            mostrarAlerta("Éxito", "Colección eliminada correctamente.", Alert.AlertType.INFORMATION);
+        } catch (IOException e) {
+            mostrarAlerta("Error", "No se pudo eliminar la colección: " + e.getMessage(), Alert.AlertType.ERROR);
+        }
+    }
+
+    public List<String> obtenerColecciones() {
+        List<String> colecciones = new ArrayList<>();
+        try {
+            String xquery = "db:list('libreria')";
+            String resultado = ejecutarConsulta(xquery);
+
+            if (resultado != null && !resultado.isEmpty()) {
+                colecciones.addAll(Arrays.asList(resultado.split("\n")));
+            }
+        } catch (Exception e) {
+            mostrarAlerta("Error", "No se pudieron obtener las colecciones.", Alert.AlertType.ERROR);
+        }
+        return colecciones;
+    }
+
 
     private void mostrarAlerta(String titulo, String mensaje, Alert.AlertType tipo) {
         Alert alerta = new Alert(tipo);
--- src/main/java/org/example/accesodatosfinal/MenuController.java
+++ src/main/java/org/example/accesodatosfinal/MenuController.java
@@ -6,9 +6,11 @@ import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
+import java.io.File;
 
 
 public class MenuController {
@@ -24,7 +26,11 @@ public class MenuController {
     }
 
     public void abrirFormularioAgregar() {
-        Stage ventanaAgregar = crearVentana("Agregar Libro", 300, 300);
+        Stage ventanaAgregar = crearVentana("Agregar Libro", 300, 350);
+
+        ComboBox<String> comboColeccion = new ComboBox<>();
+        comboColeccion.setPromptText("Seleccionar Colección");
+        comboColeccion.getItems().addAll(libreria.obtenerColecciones());
 
         TextField campoTitulo = new TextField();
         campoTitulo.setPromptText("Título");
@@ -40,20 +46,23 @@ public class MenuController {
 
         Button botonGuardar = new Button("Guardar");
         botonGuardar.setOnAction(e -> {
+            String coleccionSeleccionada = comboColeccion.getValue();
             String titulo = campoTitulo.getText();
             String autor = campoAutor.getText();
             String genero = campoGenero.getText();
             String precioTexto = campoPrecio.getText();
 
-            if (titulo.isEmpty() || autor.isEmpty() || genero.isEmpty() || precioTexto.isEmpty()) {
-                mostrarAlerta("Error", "Por favor, rellene todos los campos.", Alert.AlertType.ERROR);
+            if (coleccionSeleccionada == null || titulo.isEmpty() || autor.isEmpty() || genero.isEmpty() || precioTexto.isEmpty()) {
+                mostrarAlerta("Error", "Por favor, seleccione una colección y rellene todos los campos.", Alert.AlertType.ERROR);
                 return;
             }
 
             try {
                 double precio = Double.parseDouble(precioTexto);
                 String libroXML = "<Libro><Titulo>" + titulo + "</Titulo><Autor>" + autor + "</Autor><Genero>" + genero + "</Genero><Precio>" + precio + "</Precio></Libro>";
-                libreria.insertarLibro(libroXML);
+
+                libreria.insertarLibro(coleccionSeleccionada, libroXML);
+
                 ventanaAgregar.close();
             } catch (NumberFormatException ex) {
                 mostrarAlerta("Error", "El precio debe ser un número válido.", Alert.AlertType.ERROR);
@@ -63,7 +72,7 @@ public class MenuController {
         Button botonCancelar = new Button("Cancelar");
         botonCancelar.setOnAction(e -> ventanaAgregar.close());
 
-        VBox layout = new VBox(10, campoTitulo, campoAutor, campoGenero, campoPrecio, botonGuardar, botonCancelar);
+        VBox layout = new VBox(10, comboColeccion, campoTitulo, campoAutor, campoGenero, campoPrecio, botonGuardar, botonCancelar);
         layout.setStyle("-fx-padding: 20; -fx-alignment: center;");
 
         ventanaAgregar.setScene(new Scene(layout));
@@ -71,40 +80,13 @@ public class MenuController {
     }
 
 
-    public void abrirFormularioBuscar() {
-        Stage ventanaBuscar = crearVentana("Buscar Libro", 300, 200);
-
-        TextField campoTitulo = new TextField();
-        campoTitulo.setPromptText("Título del Libro");
-
-        Button botonBuscar = new Button("Buscar");
-        botonBuscar.setOnAction(e -> {
-            String titulo = campoTitulo.getText();
-            if (titulo.isEmpty()) {
-                mostrarAlerta("Error", "Ingrese un título.", Alert.AlertType.ERROR);
-                return;
-            }
-
-            String resultado = libreria.obtenerLibroPorTitulo(titulo);
-
-            if (resultado != null && !resultado.isEmpty()) {
-                mostrarAlerta("Libro encontrado", resultado, Alert.AlertType.INFORMATION);
-            } else {
-                mostrarAlerta("No encontrado", "No se encontró un libro con el título: " + titulo, Alert.AlertType.WARNING);
-            }
-
-            ventanaBuscar.close();
-        });
-
-        VBox layout = new VBox(10, campoTitulo, botonBuscar);
-        layout.setStyle("-fx-padding: 20; -fx-alignment: center;");
-
-        ventanaBuscar.setScene(new Scene(layout));
-        ventanaBuscar.showAndWait();
-    }
 
     public void abrirFormularioActualizar() {
         Stage ventanaActualizar = crearVentana("Actualizar Libro", 300, 250);
+
+        ComboBox<String> comboColeccion = new ComboBox<>();
+        comboColeccion.setPromptText("Seleccionar Colección");
+        comboColeccion.getItems().addAll(libreria.obtenerColecciones());
 
         TextField campoTitulo = new TextField();
         campoTitulo.setPromptText("Título del Libro");
@@ -114,17 +96,18 @@ public class MenuController {
 
         Button botonActualizar = new Button("Actualizar");
         botonActualizar.setOnAction(e -> {
+            String coleccionSeleccionada = comboColeccion.getValue();
             String titulo = campoTitulo.getText();
             String precioTexto = campoPrecio.getText();
 
-            if (titulo.isEmpty() || precioTexto.isEmpty()) {
+            if (coleccionSeleccionada == null || titulo.isEmpty() || precioTexto.isEmpty()) {
                 mostrarAlerta("Error", "Por favor, rellene todos los campos.", Alert.AlertType.ERROR);
                 return;
             }
 
             try {
                 double nuevoPrecio = Double.parseDouble(precioTexto);
-                libreria.actualizarPrecio(titulo, nuevoPrecio);
+                libreria.actualizarPrecio(coleccionSeleccionada,titulo, nuevoPrecio);
             } catch (NumberFormatException ex) {
                 mostrarAlerta("Error", "El precio debe ser un número válido.", Alert.AlertType.ERROR);
             }
@@ -132,7 +115,7 @@ public class MenuController {
             ventanaActualizar.close();
         });
 
-        VBox layout = new VBox(10, campoTitulo, campoPrecio, botonActualizar);
+        VBox layout = new VBox(10,comboColeccion, campoTitulo, campoPrecio, botonActualizar);
         layout.setStyle("-fx-padding: 20; -fx-alignment: center;");
 
         ventanaActualizar.setScene(new Scene(layout));
@@ -142,23 +125,29 @@ public class MenuController {
     public void abrirFormularioEliminar() {
         Stage ventanaEliminar = crearVentana("Eliminar Libro", 300, 200);
 
+        ComboBox<String> comboColeccion = new ComboBox<>();
+        comboColeccion.setPromptText("Seleccionar Colección");
+        comboColeccion.getItems().addAll(libreria.obtenerColecciones());
+
         Label labelTitulo = new Label("Titulo del Libro");
         TextField campoTitulo = new TextField();
         campoTitulo.setPromptText("Título del Libro");
 
         Button botonEliminar = new Button("Eliminar");
         botonEliminar.setOnAction(e -> {
+            String coleccionSeleccionada = comboColeccion.getValue();
             String titulo = campoTitulo.getText();
-            if (titulo.isEmpty()) {
+
+            if (coleccionSeleccionada == null || titulo.isEmpty()) {
                 mostrarAlerta("Error", "Ingrese un título.", Alert.AlertType.ERROR);
                 return;
             }
 
-            libreria.eliminarLibro(titulo);
+            libreria.eliminarLibro(coleccionSeleccionada ,titulo);
             ventanaEliminar.close();
         });
 
-        VBox layout = new VBox(10, labelTitulo,campoTitulo, botonEliminar);
+        VBox layout = new VBox(10, comboColeccion,labelTitulo,campoTitulo, botonEliminar);
         layout.setStyle("-fx-padding: 20; -fx-alignment: center;");
 
         ventanaEliminar.setScene(new Scene(layout));
@@ -168,11 +157,16 @@ public class MenuController {
     public void abrirFormularioListar() {
         Stage ventanaListar = crearVentana("Lista de Libros", 900, 600);
 
+        ComboBox<String> comboColeccion = new ComboBox<>();
+        comboColeccion.setPromptText("Seleccionar Colección");
+        comboColeccion.getItems().addAll(libreria.obtenerColecciones());
+
         ListView<String> listViewLibros = new ListView<>();
         Button botonCargar = new Button("Mostrar Libros");
 
         botonCargar.setOnAction(e -> {
-            String resultado = libreria.listarLibros();
+            String coleccionSeleccionada = comboColeccion.getValue();
+            String resultado = libreria.listarLibros(coleccionSeleccionada);
             listViewLibros.getItems().clear();
             if (resultado != null && !resultado.isEmpty()) {
                 String[] libros = resultado.split("\n");
@@ -182,11 +176,62 @@ public class MenuController {
             }
         });
 
-        VBox layout = new VBox(10, botonCargar, listViewLibros);
+        VBox layout = new VBox(10, comboColeccion,botonCargar, listViewLibros);
         layout.setStyle("-fx-padding: 20; -fx-alignment: center;");
 
         ventanaListar.setScene(new Scene(layout));
         ventanaListar.showAndWait();
+    }
+
+    public void abrirFormularioColecciones() {
+        Stage ventana = new Stage();
+        ventana.setTitle("Gestión de Colecciones");
+
+        TextField campoNombreColeccion = new TextField();
+        campoNombreColeccion.setPromptText("Nombre de la Colección");
+
+        Button botonSeleccionarArchivo = new Button("Seleccionar Archivo XML");
+        Label etiquetaArchivo = new Label("No se ha seleccionado ningún archivo");
+
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Archivos XML", "*.xml"));
+
+        final File[] archivoSeleccionado = {null};
+
+        botonSeleccionarArchivo.setOnAction(e -> {
+            File archivo = fileChooser.showOpenDialog(ventana);
+            if (archivo != null) {
+                archivoSeleccionado[0] = archivo;
+                etiquetaArchivo.setText("Archivo: " + archivo.getName());
+            }
+        });
+
+        Button botonAgregar = new Button("Añadir Documento a Colección");
+        botonAgregar.setOnAction(e -> {
+            String nombreColeccion = campoNombreColeccion.getText();
+            if (nombreColeccion.isEmpty() || archivoSeleccionado[0] == null) {
+                mostrarAlerta("Error", "Debe ingresar un nombre de colección y seleccionar un archivo.", Alert.AlertType.ERROR);
+                return;
+            }
+            libreria.agregarDocumentoAColeccion(nombreColeccion, archivoSeleccionado[0].getAbsolutePath());
+        });
+
+        Button botonEliminar = new Button("Eliminar Colección");
+        botonEliminar.setOnAction(e -> {
+            String nombreColeccion = campoNombreColeccion.getText();
+            if (nombreColeccion.isEmpty()) {
+                mostrarAlerta("Error", "Ingrese un nombre de colección.", Alert.AlertType.ERROR);
+                return;
+            }
+            libreria.eliminarColeccion(nombreColeccion);
+        });
+
+        VBox layout = new VBox(10, campoNombreColeccion, botonSeleccionarArchivo, etiquetaArchivo, botonAgregar, botonEliminar);
+        layout.setStyle("-fx-padding: 20; -fx-alignment: center;");
+
+        Scene scene = new Scene(layout, 400, 250);
+        ventana.setScene(scene);
+        ventana.showAndWait();
     }
 
     public void cerrarMenu() {
